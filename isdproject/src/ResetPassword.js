22782b126024eca53fb26faf8a4fb34eea4e7a60
valerieeesss <100663256+valerieeesss@users.noreply.github.com>
2024-04-03 00:27:47 +1100

    NAVBAR!!!!

--- isdproject/src/ResetPassword.js
+++ isdproject/src/ResetPassword.js
@@ -2,7 +2,59 @@ import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 
+const NavigationBar = () => {
+  const navBarStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    padding: '1rem',
+    boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+    zIndex: 1000,
+  };
+//Navigation container style
+  const navContainerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    maxWidth: '1200px',
+  };
+//Navigation item style
+  const navItemStyle = {
+    margin: '0 15px',
+    color: '#0047ab',
+    textDecoration: 'none',
+  };
 
+  return (
+    <nav style={navBarStyle}>
+      <div style={navContainerStyle}>
+        <div></div> {/* The empty div on the left is used to balance the layout */}
+        {/* Middle navigation link */}
+        <div>
+          <a href="/menu" style={navItemStyle}>Menu</a>
+          <a href="/contact" style={navItemStyle}>Contact</a>
+          <a href="/about" style={navItemStyle}>About Us</a>
+        </div>
+        {/* Login and registration links on the right */}
+        <div>
+          <a href="/login" style={navItemStyle}>Login</a>
+          <a href="/SignUp" style={navItemStyle}>Register</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+
+
+
+
+  
 const ResetPassword = () => {
   const { token } = useParams();
   const [isValidHash, setIsValidHash] = useState(false);
@@ -36,11 +88,13 @@ const ResetPassword = () => {
 
   return (
     <div>
+         <NavigationBar />
       {isValidHash && !passwordResetSuccess ? (
         <div style={containerStyle}>
           <h2 style={headingStyle}>Reset Password</h2>
           <p style={textStyle}>Resetting password for user: {userEmail}</p>
           <ResetPasswordForm onSubmit={handleResetPassword} />
+          
         </div>
       ) : isValidHash && passwordResetSuccess ? (
         <div style={containerStyle}>
@@ -100,11 +154,14 @@ const containerStyle = {
 const headingStyle = {
   fontSize: '2rem',
   marginBottom: '1rem',
+  fontWeight: 'bold',
+
 };
 
 const textStyle = {
   fontSize: '1rem',
   marginBottom: '1rem',
+  fontWeight: 'bold',
 };
 
 const linkStyle = {
