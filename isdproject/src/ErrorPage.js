05c38c6a798745fa50b755920073eceaa9c2a6aa
Adam2221114 <148194037+Adam2221114@users.noreply.github.com>
2024-04-03 09:47:53 +0800

    Update ErrorPage.js

--- isdproject/src/ErrorPage.js
+++ isdproject/src/ErrorPage.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function ErrorPage() {
+const ErrorPage = () => {
+  const style = {
+    textAlign: 'center',
+    marginTop: '50px',
+  };
+
+  const imgStyle = {
+    maxWidth: '60%',
+    height: 'auto',
+    margin: '20px auto', 
+    display: 'block', 
+  };
+
+  const linkStyle = {
+    display: 'block', 
+    marginTop: '20px',
+    fontSize: '20px',
+    color: '#007bff',
+    textDecoration: 'none',
+    padding: '10px 20px',
+    border: '1px solid #007bff',
+    borderRadius: '5px',
+    margin: '20px auto', 
+    width: 'fit-content', 
+  };
 
   return (
-    <h1>
-      Error
-    </h1>
+    <div style={style}>
+      <p style={{ fontSize: '24px' }}>404 Not Found</p>
+      <img src={`${process.env.PUBLIC_URL}/404image.png`} alt="Error" style={imgStyle} />
+      <Link to="/" style={linkStyle}>Return to welcomePage</Link>
+    </div>
   );
-}
+};
 
 export default ErrorPage;
