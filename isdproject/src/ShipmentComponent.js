dbfc50a3117c33fd8e190c70e18dc3e34e8fc3b9
Tom <8527295+OfficialTomJ@users.noreply.github.com>
2024-05-06 23:38:31 +1000

    Checkout fix

--- isdproject/src/ShipmentComponent.js
+++ isdproject/src/ShipmentComponent.js
@@ -3,35 +3,42 @@ import Cookies from "js-cookie";
 import axios from "axios";
 
 const ShippingComponent = () => {
+  const [addresses, setAddresses] = useState([]);
+  const [selectedAddress, setSelectedAddress] = useState("");
 
-    const [addresses, setAddresses] = useState([]);
+  const fetchUserAddresses = async () => {
+    const token = Cookies.get("token");
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/shipment/user-addresses",
+        {
+          headers: {
+            Authorization: `${token}`,
+          },
+        }
+      );
+      setAddresses(response.data.addresses);
+    } catch (error) {
+      console.error("Error fetching user addresses:", error);
+    }
+  };
 
-    const fetchUserAddresses = async () => {
-      const token = Cookies.get("token");
-      try {
-        const response = await axios.get(
-          "http://localhost:8080/api/shipment/user-addresses",
-          {
-            headers: {
-              Authorization: `${token}`,
-            },
-          }
-        );
-        setAddresses(response.data.addresses);
-      } catch (error) {
-        console.error("Error fetching user addresses:", error);
-      }
-    };
+  useEffect(() => {
+    fetchUserAddresses();
+  }, []);
 
-    useEffect(() => {
-      fetchUserAddresses();
-    }, []);
-
+  const handleAddressChange = (e) => {
+    setSelectedAddress(e.target.value);
+  };
 
   return (
     <>
       <h3>Choose Shipping Address</h3>
-      <select style={{ marginBottom: "10px", width: "100%", padding: "10px" }}>
+      <select
+        style={{ marginBottom: "10px", width: "100%", padding: "10px" }}
+        onChange={handleAddressChange}
+        value={selectedAddress}
+      >
         <option value="">Select a saved shipping address</option>
         {addresses.map((address, index) => (
           <option key={index} value={address}>
@@ -39,12 +46,18 @@ const ShippingComponent = () => {
           </option>
         ))}
       </select>
-      <hr style={{ marginBottom: "10px", borderTop: "1px solid #eaeaea" }} />
-      <h3>Or enter a new shipping address</h3>
-      <textarea
-        placeholder="Enter shipping address..."
-        style={{ marginBottom: "20px", width: "100%", padding: "10px" }}
-      ></textarea>
+      {selectedAddress === "" && (
+        <>
+          <hr
+            style={{ marginBottom: "10px", borderTop: "1px solid #eaeaea" }}
+          />
+          <h3>Or enter a new shipping address</h3>
+          <textarea
+            placeholder="Enter shipping address..."
+            style={{ marginBottom: "20px", width: "100%", padding: "10px" }}
+          ></textarea>
+        </>
+      )}
       <hr style={{ marginBottom: "10px", borderTop: "1px solid #eaeaea" }} />
       <h3>Choose Shipping Method</h3>
       <select
