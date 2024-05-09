72743d65fddc4e1e0d44c5bdb6d471185d2e4dfe
snair2029 <shreya.nair@student.uts.edu.au>
2024-05-09 11:22:04 +1000

    Order controller/route
    CHanged code

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,45 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Order = require('../models/Order'); // Import Order model
+const orderController = require('../controllers/orderController');
+const auth = require('../middleware/auth');
 
 // Route to create a new order
-router.post('/create-order', async (req, res) => {
-  try {
-    // Create a new order object based on request body
-    const newOrder = new Order(req.body);
-
-    // Save the order to the database
-    await newOrder.save();
-
-    res.status(201).json(newOrder); // Respond with the newly created order
-  } catch (error) {
-    console.error('Error creating order:', error);
-    res.status(500).json({ error: 'Failed to create order' });
-  }
-});
+router.post('/create-order', auth, orderController.createOrder);
 
 // Route to update an existing order
-router.put('/update-order/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedOrder = await Order.findByIdAndUpdate(id, req.body, { new: true });
-    res.json(updatedOrder);
-  } catch (error) {
-    console.error('Error updating order:', error);
-    res.status(500).json({ error: 'Failed to update order' });
-  }
-});
+router.put('/update-order/:id', auth, orderController.updateOrder);
 
 // Route to delete an existing order
-router.delete('/delete-order/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    await Order.findByIdAndDelete(id);
-    res.status(204).send(); // No content response
-  } catch (error) {
-    console.error('Error deleting order:', error);
-    res.status(500).json({ error: 'Failed to delete order' });
-  }
-});
+router.delete('/delete-order/:id', auth, orderController.deleteOrder);
 
 module.exports = router;
