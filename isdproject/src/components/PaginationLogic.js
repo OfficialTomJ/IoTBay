cac5d9ec83cfcf79a4df69aa41199fe19a7fc2aa
ST3427 <SubinRatna.Thapa@student.uts.edu.au>
2024-05-10 19:09:48 +1000

    created Paginationlogic.js

---
+++ isdproject/src/components/PaginationLogic.js
@@ -0,0 +1,22 @@
+const PaginationLogic = ({ totalPages, currentPage }) => {
+    let startPage, endPage;
+    if (totalPages <= 5) {
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      if (currentPage <= 3) {
+        startPage = 1;
+        endPage = 5;
+      } else if (currentPage + 1 >= totalPages) {
+        startPage = totalPages - 4;
+        endPage = totalPages;
+      } else {
+        startPage = currentPage - 2;
+        endPage = currentPage + 2;
+      }
+    }
+  
+    return { startPage, endPage };
+  };
+  
+  export default PaginationLogic;  
