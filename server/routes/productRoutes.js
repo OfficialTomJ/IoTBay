a528b60f5f8a32cc81c2ab597d62f229e46c57e5
ST3427 <SubinRatna.Thapa@student.uts.edu.au>
2024-05-10 18:33:30 +1000

    modified routes.js and made the database running again

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -5,15 +5,13 @@ const auth = require('../middleware/auth');
 const productController = require('../controllers/productController');
 
 router.post('/create', productController.createProduct);
-router.get('/productid', auth, productController.getProductid);
+router.get('/product', auth, productController.getProduct);
 router.delete('/delete', auth, productController.deleteProduct);
 router.put('/update', auth, productController.updateProduct);
 
 router.get('/product/:id', auth, productController.getProductById);
 router.get('/products', (req, res) => {
-    // Your route logic for /products here
-    // For example, you can send a response or query the database
-    // Replace this comment with your actual logic
+
     res.send('This is the /products route');
 });
 
