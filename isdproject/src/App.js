ddef44164ebaa3f8406a5cd54a4863a4826330d8
ST3427 <SubinRatna.Thapa@student.uts.edu.au>
2024-05-10 19:26:12 +1000

    modified App.js

--- isdproject/src/App.js
+++ isdproject/src/App.js
@@ -1,3 +1,4 @@
+import './tailwind.css';
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -8,8 +9,10 @@ import Profile from './Profile';
 import ResetPassword from './ResetPassword';
 import WelcomePage from './WelcomePage';
 import LogoutPage from './Logout';
-import Product from './Product';
-
+import ProductPage from './ProductPage';
+import ShoppingCart from './ShoppingCart';
+import Checkout from './Checkout';
+import UserAuthentication from './UserAuthentication';
 
 function App() {
   return (
@@ -19,11 +22,14 @@ function App() {
         <Route path="/" element={<WelcomePage/>} />
         <Route path="/signup" element={<Signup/>} />
         <Route path="/login" element={<Login/>} />
-        <Route path="/product" element={<Product/>} />
         <Route path="/profile" element={<Profile/>} /> {}
         <Route path="/reset-password/:token" element={<ResetPassword/>} />
         {/* Provide a 404 error page for all undefined routes */}
         <Route path="/logout" element={<LogoutPage/>} />
+        <Route path="/product" element={<ProductPage/>} />
+        <Route path="/ShoppingCart" element={<ShoppingCart/>} />
+        <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/UserAuthentication" element={<UserAuthentication/>} />
         <Route path="*" element={<ErrorPage/>} />
       </Routes>
     </Router>
